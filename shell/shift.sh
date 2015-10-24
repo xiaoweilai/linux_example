63c14e427ebe9941a63496b582fa67e372c65d34
xiaoweilai <wxjlmr@126.com>
2015-10-24 11:03:35 +0800

    add shift order test

---
+++ shell/shift.sh
@@ -0,0 +1,5 @@
+until [ $# -eq 0 ]
+do
+echo "第一个参数为: $1 参数个数为: $#"
+shift
+done
---
+++ shell/shift2.sh
@@ -0,0 +1,13 @@
+if [ $# -eq 0 ]
+then
+echo "Usage:x_shift2.sh 参数"
+exit 1
+fi
+sum=0
+until [ $# -eq 0 ]
+do
+sum=`expr $sum + $1`
+shift
+done
+echo "sum is: $sum"
+
